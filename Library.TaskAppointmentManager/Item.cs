b7e88f55734487820d4182b044539e70fe0a7f71
Jacob Gregie <90068643+mastermind583@users.noreply.github.com>
2021-10-03 16:26:22 -0400

    Add files via upload

---
+++ Library.TaskAppointmentManager/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Library.TaskAppointmentManager
+{
+    public class Item
+    {
+        private static int currentId = 1;
+
+        public Item()
+        {
+            Id = currentId++;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
