b903d08cf64e648c559fdf338979061ecab18eba
Jacob <mastermind583@gmail.com>
2021-10-13 19:16:46 -0400

    many change

--- Library.TaskAppointmentManager.UWP/ViewModels/MainViewModel.cs
+++ Library.TaskAppointmentManager.UWP/ViewModels/MainViewModel.cs
@@ -1,23 +1,40 @@
 ﻿using Library.TaskAppointmentManager.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Library.TaskAppointmentManager.ViewModels
 { 
-    public class MainViewModel
+    public class MainViewModel: INotifyPropertyChanged
     {
-        public List<Item> Items { get; set; }
+        public ObservableCollection<Item> Items { get; set; }
         public Item SelectedItem { get; set; }
         public string Query { get; set; }
-        private string persistencePath;
 
+        private string persistencePath;
+        private JsonSerializerSettings serializationSettings;
         public MainViewModel()
         {
-            Items = new List<Item>();
+            Items = new ObservableCollection<Item>();
             persistencePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            serializationSettings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+            var myItems = JsonConvert.SerializeObject(this, serializationSettings);
+            File.WriteAllText($"{persistencePath}\\SaveData.txt", myItems);
+            var myItemDeserialized = JsonConvert.DeserializeObject<Item>(myItems, serializationSettings);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         public void AddItem()
@@ -34,6 +51,18 @@ namespace Library.TaskAppointmentManager.ViewModels
             {
                 Items.Remove(SelectedItem);
             }
+        }
+
+        public async Task EditItem()
+        {
+            var diag = new ItemDialog(Items, SelectedItem);
+            NotifyPropertyChanged("SelectedItem");
+            await diag.ShowAsync();
+        }
+
+        public async Task Search()
+        {
+            Console.WriteLine(Query);
         }
     }
 }
