1588d3881308266a8e6ddb656c78ee0c3f8d2bf3
Jacob Gregie <mastermind583@gmail.com>
2021-11-14 18:06:25 -0500

    m

--- TaskAppointmentManager.API/TaskAppointmentManager.API/Persistence/Database.cs
+++ TaskAppointmentManager.API/TaskAppointmentManager.API/Persistence/Database.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using Library.TaskAppointmentManager.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TaskAppointmentManager.API.Persistence
+namespace Api.TaskAppointmentManager.Persistence
 {
-    public class Database
+    public static class Database
     {
+        public static ObservableCollection<Library.TaskAppointmentManager.Models.Task> ToDos { get; set; } = new ObservableCollection<Library.TaskAppointmentManager.Models.Task> {
+            new Library.TaskAppointmentManager.Models.Task { Name = "First",
+                       Description = "First ToDo"},
+            new Library.TaskAppointmentManager.Models.Task { Name = "Second",
+                       Description = "Second ToDo"}
+        };
+        public static ObservableCollection<Appointment> Appointments { get; set; } = new ObservableCollection<Appointment>
+        {
+            new Appointment { Name = "1st",
+                       Description = "First Appointment"},
+            new Appointment { Name = "2nd",
+                       Description = "Second Appointment"}
+        };
     }
 }
